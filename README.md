d924abbe09851579535ff9e0a32446369299d89d
vombagel64 <Moodysnowman@outlook.com>
2026-05-02 17:44:46 -0400

    changed the cursor and fixed some stupid code mistakes. also added a new logo and a todo file

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # vomOS
 DISCLAIMER: this project is going to have VERY slow progress. right now, im still learning Lua and figuring out Love2D, and every new feature or update I commit is me using something I just learned, or in some cases, changing code because of something I learned. I hope you understand and your patience and feedback are very much appreciated!
-
-for now, you can view the files in the project. some of them may not have anything inside, but they're here to show the way i plan to organize this project. as it says in the description, I want to design vomOS do be a modular toy OS that allows you to easily make your own apps for it*. it sounds ambitious, and it really is, at least for me. I've only recently started coding for real in Lua, so it may take a while until i gain the skills neccesary to develop this thing...
+for now, you can view the files in the project. some of them may not have anything inside, but they're here to show the way i plan to organize this project. as it says in the description, I want to design vomOS do be a modular toy OS that allows you to easily make your own apps for it*. it sounds ambitious, especially because I'm still learning Lua.  Like I said earlier, this will make progress SUPER slow, at least for now...
 so until then, if you plan on making apps for vomOS, or just want to see what its all about, you can study the files here and... use your imagination lol
 or maybe you can use this to try and make vomOS or something similar yourself.
 either way, I hope you wont mind waiting a bit, haha
--- m_system/main.lua
+++ m_system/main.lua
@@ -3,23 +3,29 @@ local json = require("m_libraries/dkjson")
 
 function love.load()
     -- read json and decode
-    local data = love.filesystem.read("userdata.json")
-    local userdata = json.decode(data)
+    local jsondata = love.filesystem.read("userdata.json")
+    local userdata = json.decode(jsondata)
+
+    -- controls the state of vomOS. Ex: boot, app, etc.
+    local vState = "boot"
 
     canEsc = true
    
-    font = love.graphics.newFont("fonts/PetitePx.ttf", 20)
+    love.window.setFullscreen(false)
+
+    font = love.graphics.newFont("fonts/Monocraft.ttf", 20)
     love.graphics.setFont(font)
     
-    love.graphics.newImage("m_assets/vom_icon.png")
-    love.graphics.newImage("m_assets/pointerW.png")
-    love.graphics.newImage("m_assets/pointerB.png")
+    love.graphics.newImage("m_assets/vomlogo1.png")
+    love.graphics.newImage("m_assets/crosscursor.png")
+    love.graphics.newImage("m_assets/vomstartlogo.png")
+    
 
-    local data = love.image.newImageData("m_assets/pointerW.png")
-    cursor = love.mouse.newCursor(data, 0, 0)
+    local cr_data = love.image.newImageData("m_assets/crosscursor.png")
+    cursor = love.mouse.newCursor(cr_data, 6, 6)
     love.mouse.setCursor(cursor)
 
-    love.window.setIcon(love.image.newImageData("m_assets/vom_icon.png"))
+    love.window.setIcon(love.image.newImageData("m_assets/vomlogo1.png"))
     love.window.setTitle("ttt")
 end
 
@@ -28,24 +34,12 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.setBackgroundColor(0, 0, 0.4)
-    
-    love.graphics.print("vomOS test window", 0, 0)
-    love.graphics.print("Press F11 to toggle fullscreen", 0, 10)
-    love.graphics.print("Press ESC to quit", 0, 20)
+    love.graphics.setBackgroundColor(1, 1, 1)
+    love.graphics.draw(love.graphics.newImage("m_assets/vomstartlogo.png"), 0, 0)
+    love.graphics.print("press ESC to end the program", 0, 20)
 end
 
 function love.keypressed(key)
-    if key == "f11" then
-        if fscreen == false then
-            love.window.setFullscreen(true)
-            fscreen = true
-        else
-            love.window.setFullscreen(false)
-            fscreen = false
-        end
-    end
-
     if key == "escape" then
         if canEsc == true then
             love.event.quit()
